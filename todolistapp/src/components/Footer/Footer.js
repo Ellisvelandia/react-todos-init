a0971b7f570510d8c25acb08c0ccbd6fcd4af56b
ellisvelandia <eyis619@gmail.com>
2022-07-27 20:02:28 -0500

    TodoList

---
+++ todolistapp/src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p> TodoList App 2022 &copy;</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ todolistapp/src/components/Social/Social.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Social() {
+  return (
+    <div class="social">
+      <i class="fa fa-apple" id="apple"></i>
+      <i class="fa fa-twitter" id="twitter"></i>
+      <a href="https://github.com/Ellisvelandia" target="_blank">
+        <i class="fa fa-github-square github" id="github"></i>
+      </a>
+      <i class="fa fa-facebook-square" id="facebook"></i>
+    </div>
+  );
+}
+
+export default Social;
